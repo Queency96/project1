c6db55a8ed52299e45eab62a79619e92767745ba
Queency96 <queency96@gmail.com>
2024-02-29 07:11:25 +0100

    added active link in navbar

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,3 +1,13 @@
-export default function Customers(){
-    return <h1>Page one</h1>
+export default function Customers() {
+  return (
+    <>
+      <h1>Second page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia
+        praesentium deserunt necessitatibus cupiditate numquam sapiente enim,
+        sit deleniti, ullam quaerat inventore in? Voluptatem repellat excepturi
+        pariatur ex est ipsum obcaecati?
+      </p>
+    </>
+  )
 }
